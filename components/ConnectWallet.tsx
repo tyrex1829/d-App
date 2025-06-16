e3c07877b3cf629b03140b6fb39347ea3675487f
Saksham Khare <sakshamkhare19@gmail.com>
2025-06-16 16:50:45 +0530

    Fix: fetch balance correctly with useEffect, render correct components for use

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -5,22 +5,26 @@ import {
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 import Airdrop from "./Airdrop";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import SendTokens from "./SendTokens";
+import SignMessage from "./SignMessage";
 
 const ConnectWallet = () => {
   const [balance, setBalance] = useState(0);
   const wallet = useWallet();
   const { connection } = useConnection();
 
-  async function getBalance() {
-    if (wallet.publicKey) {
-      const balanceUser = await connection.getBalance(wallet.publicKey);
-      setBalance(balanceUser);
-    }
-  }
+  useEffect(() => {
+    const getBalance = async () => {
+      if (!wallet.publicKey) return;
+      const balance = await connection.getBalance(wallet.publicKey);
+      setBalance(balance);
+    };
 
-  getBalance();
+    getBalance();
+  }, [wallet.publicKey, connection]);
 
   return (
     <div className="flex flex-col justify-center gap-20 pt-12">
@@ -28,7 +32,7 @@ const ConnectWallet = () => {
         <WalletMultiButton />
         <WalletDisconnectButton />
       </div>
-      <div className="flex items-center gap-32">
+      <div className="flex items-center gap-12">
         <div className="border border-gray-400 rounded-xl px-10 py-20 text-xl font-medium flex flex-col gap-3">
           <div className="flex items-center gap-3">
             Public Key :{" "}
@@ -36,9 +40,16 @@ const ConnectWallet = () => {
               {wallet.publicKey?.toString()}
             </span>
           </div>
-          <div>Show Balance : {balance}</div>
+          <div className="flex items-center gap-3">
+            Balance :
+            <span className="font-normal text-gray-700">
+              {balance / LAMPORTS_PER_SOL} SOL
+            </span>
+          </div>
         </div>
         <Airdrop />
+        <SendTokens />
+        <SignMessage />
       </div>
     </div>
   );
